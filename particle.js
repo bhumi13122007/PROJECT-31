a87090b3ed2f497d270bf05488b6aad7b62a0320
bhumi13122007 <75309907+bhumi13122007@users.noreply.github.com>
2021-06-10 21:29:55 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,25 @@
+class particle{
+    constructor(x,y,w,h){
+        var options = {
+            isStatic: true
+        }
+        this.body=Bodies.circle(x,y,this,r,options);
+this.color=color(random(0,255),random(0,255),random(0,255));
+World.add(world,this.body);
+
+    }
+    display(){
+
+  var pos = this.body.position;
+        rectMode(CENTER);
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+         noStroke();
+        fill("white");
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
+        pop();
+
+    }
+}
